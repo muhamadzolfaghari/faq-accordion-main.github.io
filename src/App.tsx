ae0376d55d985335842df73c300c5583b942cfbc
Mohammad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-12-05 20:37:52 +0330

    initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+const data: { category: string; question: string }[] = [
+  { category: "", question: "" },
+];
+
 export default function Home() {
+  const a = "آیا شما با جسم خود راحت هستید و خیلی کم مریض می شوید؟";
+
   return <>12</>;
 }
